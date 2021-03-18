de9f0341940cb1a72e055299edd5c3ea7f810dc9
NeliusFox <79461935+NeliusFox@users.noreply.github.com>
2021-03-18 11:01:31 +0100

    Update flooetileset2.tsx

--- flooetileset2.tsx
+++ flooetileset2.tsx
@@ -1,6 +1,36 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="2021.02.15" name="floortileset" tilewidth="32" tileheight="32" spacing="2" margin="1" tilecount="64" columns="8">
  <image source="floortileset.png" width="272" height="272"/>
+ <tile id="0">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
  <tile id="37">
   <properties>
    <property name="collides" type="bool" value="true"/>
